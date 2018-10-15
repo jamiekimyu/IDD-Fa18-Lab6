0641b3c7b9ca7ec356a32880a6ef110e8242068b
Jamie Yu <jme.kim.yu@gmail.com>
2018-10-15 16:46:15 -0700

    updated chatServer

--- chatServer.js
+++ chatServer.js
@@ -9,7 +9,8 @@ var app = express(); // webapp
 var http = require('http').Server(app); // connects http library to server
 var io = require('socket.io')(http); // connect websocket library to server
 var serverPort = 8000;
-
+var moment = require('moment');
+moment().format();
 
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
 // use express to create the simple webapp
@@ -30,7 +31,7 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
 
-    socket.emit('answer', "Hey, hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
+    socket.emit('answer', "Hey, hello I am travelHelper, a simple chat bot."); //We start with the introduction;
     setTimeout(timedQuestion, 5000, socket, "What is your name?"); // Wait a moment and respond with a question.
 
   });
@@ -53,21 +54,51 @@ function bot(data, socket, questionNum) {
   if (questionNum == 0) {
     answer = 'Hello ' + input + ' :-)'; // output response
     waitTime = 5000;
-    question = 'How old are you?'; // load next question
+    question = 'When are you going traveling? (month/day/year format please)'; // load next question
   } else if (questionNum == 1) {
-    answer = 'Really, ' + input + ' years old? So that means you were born in: ' + (2018 - parseInt(input)); // output response
+    let firstDate = moment(input, 'M/D/YYYY');
+    let secondDate = moment(new Date().toLocaleString().split(',')[0], 'M/D/YYYY');
+    console.log('Second'+ secondDate);
+    console.log('First' + firstDate);
+    var timeDifference =  firstDate.diff(secondDate, 'days');
+    answer = 'Cool, that means you will leave in ' + timeDifference + ' days';//output response
+   // answer = 'Really, ' + input + ' years old? So that means you were born in: ' + (2018 - parseInt(input)); // output response
     waitTime = 5000;
-    question = 'Where do you live?'; // load next question
+    question = 'Where are you going?'; // load next question
   } else if (questionNum == 2) {
     answer = 'Cool! I have never been to ' + input + '.';
     waitTime = 5000;
-    question = 'Whats your favorite color?'; // load next question
-  } else if (questionNum == 3) {
+    question = 'It seems like it will be cold when you get there if you look up the weather. Did you pack your jacket?' //load next question
+  } else if (questionNum == 3) { 
+    if (input.toLowerCase() === 'yes') 
+     answer = 'Cool! Good Job :)';
+    else 
+     answer = 'Pack your jacket! it really looks like it will be freezing';
+	
+//     waitTime = 5000;
+//     questionNum++;
+//   } else if (input.toLowerCase() === 'no') {
+//     answer = 'Pack your jacket! It really looks like it will be freezing! I wont budge until you say that you packed your jacket';
+//     waitTime = 0; 
+//     questionNum--; //Here we go back in  the question number this can end up in a loop
+//   } else {
+//     question = 'Did you look up the weather and see how cold it will be?';
+//     answer = 'I did not understand you. Could you please answer "yes" or "no"?';
+//     questionNum--;
+//     waitTime = 5000;
+     waitTime = 5000;
+//    question = '';
+//   } 
+//  } else if (questionNum == 4) {
+//    answer = 'Good Job for packing warmly.';
+//    waitTime = 5000;	
+    question = 'Just curious, whats the color of your jacket?'; // load next question
+  } else if (questionNum == 4) {
     answer = 'Ok, ' + input + ' it is.';
     socket.emit('changeBG', input.toLowerCase());
     waitTime = 5000;
     question = 'Can you still read the font?'; // load next question
-  } else if (questionNum == 4) {
+  } else if (questionNum == 5) {
     if (input.toLowerCase() === 'yes' || input === 1) {
       answer = 'Perfect!';
       waitTime = 5000;
